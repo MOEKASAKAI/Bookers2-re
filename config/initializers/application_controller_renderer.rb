e84533232c5c30977b98da9d44a99675f1ae32d5
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-20 13:48:03 +0000

    [Add] user編集機能の追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'b65156c2af7709be6b4e5f5f291c1b5f55b21bf1f9428dae24492c51b6b64af13b91bb156640797d5712386e09e539953b2cc271a935460697524727bcaa59bc'
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'homes#top'
   get 'homes/about'
-  resources :users, only: [:show]
+  resources :users, only: [:show, :index, :edit, :update]
 end
