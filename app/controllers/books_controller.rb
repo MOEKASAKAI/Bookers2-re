3c7783f46860374170f3dfc8d1b13d9460f0a78f
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-22 13:51:34 +0000

    [Add] Book投稿機能・一覧詳細画面

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,24 +1,37 @@
 class BooksController < ApplicationController
-  
+
   def index
+    @books = Book.all
   end
-  
+
   def show
+    @book = Book.find(params[:id])
   end
-  
+
   def new
+    @book = Book.new
   end
-  
+
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
-  
+
   def edit
   end
-  
+
   def update
   end
-  
+
   def destroy
   end
-  
+
+  private
+
+  def book_params
+    params.permit(:title, :body)
+  end
+
 end
