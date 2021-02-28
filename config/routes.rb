26bd492fee86c94f74016244b63f6bdee2b33583
MOEKASAKAI <wpi.floves2@gmail.com>
2021-02-28 05:41:53 +0000

    [Add] BookCommentsコントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,5 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users, only: [:show, :index, :edit, :update]
   resources :books, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+    resources :book_comments, only: [:create, :destroy]
 end
